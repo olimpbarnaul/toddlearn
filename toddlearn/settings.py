10dff68b6e526b44dd52b557e11f7ea861833341
OlimpBarnaul <olimpbarnaul@mail.ru>
2021-06-02 21:45:34 +0700

    -

--- toddlearn/settings.py
+++ toddlearn/settings.py
@@ -127,7 +127,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "templates/static")]
+STATIC_ROOT = 'static'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'templates', 'static')]
 
 INTERNAL_IPS = env.list('INTERNAL_IPS', default=[])
 
