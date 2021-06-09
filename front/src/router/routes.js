30b103785a3435ecd627dac89a19c4b1f3b5f7d9
OlimpBarnaul <olimpbarnaul@mail.ru>
2021-06-09 23:02:08 +0700

    vue router installed

---
+++ front/src/router/routes.js
@@ -0,0 +1,19 @@
+export default [
+  {
+    path: "/",
+    name: "Home",
+    component: importComponent("Home"),
+  },
+  {
+    path: "/about",
+    name: "About",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: importComponent("About"),
+  },
+];
+
+function importComponent(componentName) {
+  return () => import("../views/" + componentName + ".vue");
+}
