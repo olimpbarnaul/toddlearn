43669cfd0be72aceaace0a5c7a6fe5fca22e67e7
OlimpBarnaul <olimpbarnaul@mail.ru>
2021-06-09 20:49:47 +0700

    vue-router & tailwindcss added

--- front/src/main.js
+++ front/src/main.js
@@ -1,8 +1,25 @@
 import Vue from "vue";
-import App from "./App.vue";
+import routes from "./routes";
+import "./assets/tailwind.css";
 
-Vue.config.productionTip = false;
+const app = new Vue({
+  el: "#app",
+  data: {
+    currentRoute: window.location.pathname,
+  },
+  computed: {
+    ViewComponent() {
+      const matchingView = routes[this.currentRoute];
+      return matchingView
+        ? require("./pages/" + matchingView + ".vue")
+        : require("./pages/404.vue");
+    },
+  },
+  render(h) {
+    return h(this.ViewComponent);
+  },
+});
 
-new Vue({
-  render: (h) => h(App),
-}).$mount("#app");
+window.addEventListener("popstate", () => {
+  app.currentRoute = window.location.pathname;
+});
