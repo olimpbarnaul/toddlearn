63bc84810784ed173b4ef78fe619fb9218385a16
OlimpBarnaul <olimpbarnaul@mail.ru>
2021-06-18 18:37:58 +0700

    test audio playing for Safari

--- front/src/plugins/playAudio.js
+++ front/src/plugins/playAudio.js
@@ -1,16 +1,16 @@
 export default async function (word, lang) {
-	return new Promise(function (resolve, reject) {
-		const audio = new Audio(); // create audio wo/ src
-		audio.preload = "auto"; // intend to play through
-		audio.autoplay = true; // autoplay when loaded
-		audio.onerror = reject; // on error, reject
-		audio.onended = resolve; // when done, resolve
+  return new Promise(function (resolve, reject) {
+    const audio = new Audio(); // create audio wo/ src
+    audio.preload = "auto"; // intend to play through
+    audio.autoplay = true; // autoplay when loaded
+    audio.onerror = reject; // on error, reject
+    audio.onended = resolve; // when done, resolve
 
-		audio.src =
-			process.env.VUE_APP_API_URL +
-			"/english/api/play?" +
-			(lang ? "lang=" + lang + "&" : "") +
-			"phrase=" +
-			word; // just for example
-	});
+    audio.src =
+      process.env.VUE_APP_API_URL +
+      "/english/api/play?" +
+      (lang ? "lang=" + lang + "&" : "") +
+      "phrase=" +
+      word; // just for example
+  });
 }
